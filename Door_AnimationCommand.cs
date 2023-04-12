422895ea3be2c9739bcf2015eecf625c29675a0a
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-04-12 12:24:58 -0400

    Add files via upload

---
+++ Door_AnimationCommand.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_AnimationCommand : MonoBehaviour
+{
+    public GameObject door;
+    public KeyCode keyBoardbutton;
+
+    private bool _isOpen;
+    private bool _isActivated;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isActivated)
+            OpeningCheck();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _isActivated = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isActivated = false;
+    }
+
+    void OpeningCheck()
+    {
+        if (Input.GetKeyDown(keyBoardbutton))
+        {
+            print("Checking");
+            _isOpen = !_isOpen;
+            if (_isOpen)
+                door.GetComponent<Animator>().SetTrigger("Opening");
+            else
+                door.GetComponent<Animator>().SetTrigger("Closing");
+        }
+    }
+
+}
---
+++ Melee_Action.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee_Action : MonoBehaviour
+{
+    public GameObject weapon;
+    public KeyCode keyBoardButton;
+
+    private bool _isActivated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyBoardButton))
+        {
+            Melee();
+        }
+    }
+    void Melee()
+    {
+        weapon.GetComponent<Animator>().SetTrigger("Action");
+    }
+}
