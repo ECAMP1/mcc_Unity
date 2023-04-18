fb5e7f3290795c8dbe4785a211283f0d42709d79
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-04-17 21:16:24 -0400

    Add files via upload

---
+++ ParticleCTRL_Flames.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCTRL_Flames : MonoBehaviour
+{
+    public ParticleSystem particles;
+    public float maxSize;
+    public float minSize;
+    public float speed;
+
+    private bool _isGrowing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles.startSize = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isGrowing)
+            LerpSize();
+        if (_isGrowing == false)
+            particles.startSize = Mathf.MoveTowards(particles.startSize, minSize, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _isGrowing = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isGrowing = false;
+    }
+
+    void LerpSize()
+    {
+        particles.startSize = Mathf.MoveTowards(particles.startSize,maxSize, speed* Time.deltaTime);
+    }
+}
