f59683d2644be423234efcff32d567b166e3cff5
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-04-21 11:10:39 -0400

    Add files via upload

---
+++ Crouch.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    public float crouchScale;
+    public KeyCode crouchButton;
+
+    private float startYScale;
+    private bool _buttonDown;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startYScale = transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyInput();
+    }
+
+    void MyInput()
+    {
+        if (Input.GetKeyDown(crouchButton))
+        {
+            _buttonDown =!_buttonDown;
+            if (_buttonDown)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, crouchScale, transform.localScale.z);
+                rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+            }
+        }
+    }
+}
