3e5bca2650c6191785f8039619c74efcb2a15130
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-04-26 13:41:00 -0400

    Add files via upload

---
+++ WaterSplash.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSplash : MonoBehaviour
+{
+    public Transform splashSpawn;
+    public ParticleSystem waterSplash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(waterSplash, splashSpawn.position, splashSpawn.rotation);
+    }
+}
