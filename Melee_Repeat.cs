59b0eac244cd641931ad165369e41f4602ca3019
ECAMP1 <33298376+ECAMP1@users.noreply.github.com>
2023-05-09 21:11:17 -0400

    Add files via upload

---
+++ Melee_Repeat.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee_Repeat : MonoBehaviour
+{
+    public GameObject weapon;
+    public KeyCode keyBoardButton;
+    public GameObject weaponTrail;
+    public float offDelay;
+
+    private bool _isMoving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponTrail.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyBoardButton))
+            _isMoving = !_isMoving;
+        if (_isMoving)
+        {
+            weapon.GetComponent<Animator>().SetBool("isMirrored", false);
+            weaponTrail.SetActive(true);
+            Invoke("trailOff", offDelay);
+        }
+        else
+            weapon.GetComponent<Animator>().SetBool("isMirrored", true);
+    }
+
+    void trailOff()
+    {
+        weaponTrail.SetActive(false);
+    }
+}
